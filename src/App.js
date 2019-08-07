2f216767151e1be3c3c4b7dffcf2efed214c2bad
Noble Millie <noble.millie@gmail.com>
2019-08-07 02:50:34 -0700

    modular refactor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
-import BarChart from './BarChart'
+// import BarChart from './BarChart'
+import ReactComponent from './ReactComponent'
 
 import './App.css'
 import './BarChart.css'
 
 import logo from './logo.svg'
+import d3Logo from './d3-logo.svg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} id="react-logo" className="App-logo" alt="react-logo" />
+        <img src={d3Logo} id="d3-logo" className="App-logo" alt="d3-logo" />
       </header>
-      <h1>D3333Z Hooks</h1>
+      <h1>sj.Eb hooks</h1>
       <h2>DOM DADA</h2>
-      <BarChart id="barchart" data={[1, 16, 3, 6, 7, 10]} />
+      <ReactComponent />
+      {/* <BarChart data={[8, 6, 23, 6, 7, 10]} width={450} height={300} /> */}
     </div>
   )
 }
--- src/BarChart.js
+++ src/BarChart.js
@@ -2,30 +2,59 @@ import React, { useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import * as d3 from 'd3'
 
-const BarChart = ({ id, data, width = 450, height = 300 }) => {
+interface IProps {
+  data?: number[];
+}
+
+// const BarChart = ({ data, width = 450, height = 300 }) => {
+const BarChart = (props: IProps) => {
+  const d3Container = useRef(null)
+
   useEffect(() => {
-    const svg = d3
-      .select('#' + id)
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height)
-      .style('border', `1px solid red`) // for dev ease
-      .style('background-color', 'lightyellow') // for dev ease
-      .style('margin-left', 10)
+    if (props.data && d3Container.current) {
+      // .select('#' + id)
+      const svg = d3
+        .select(d3Container.current)
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .style('border', `1px solid red`) // for dev ease
+        .style('background-color', 'lightyellow') // for dev ease
+        .style('margin-left', 10)
 
-    svg
-      .selectAll('rect')
-      .data(data)
-      .enter()
-      .append('rect')
-      .attr('x', (d, i) => i * 70)
-      .attr('y', (d, i) => height - 12 * d)
-      .attr('width', 55)
-      .attr('height', (d, i) => d * 12)
-      .attr('fill', 'slateblue')
-  }, [data, height, id, width])
+      const update = svg
+        .append('g')
+        .selectAll('rect')
+        .data(props.data)
 
-  return <div id={id} />
+      update
+        .enter()
+        .append('rect')
+        .attr('x', (d, i) => i * 70)
+        .attr('y', (d, i) => height - 12 * d)
+        .attr('width', 55)
+        .attr('height', (d, i) => d * 12)
+        .text(d => d)
+        .attr('fill', 'slateblue')
+
+      update
+        .attr('x', (d, i) => i * 70)
+        .attr('y', (d, i) => height - 12 * d)
+        .text(d => d)
+
+      update.exit().remove()
+    }
+  }, [props.data])
+
+  return (
+    <svg
+      className="d3-component"
+      ref={d3Container}
+      // width={450}
+      // height={300}
+      // id={id}
+    />
+  )
 }
 
 export default BarChart
